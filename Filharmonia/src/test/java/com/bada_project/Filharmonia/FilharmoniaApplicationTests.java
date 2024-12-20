97c0d2b7355419407c1c3e9e2d9bb3d8f71f4a9d
FRANCISZEK ZAREBSKI <fran.zarebski@gmail.com>
2024-12-20 18:15:41 +0100

    Add basic spring app structure

---
+++ Filharmonia/src/test/java/com/bada_project/Filharmonia/FilharmoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bada_project.Filharmonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilharmoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
