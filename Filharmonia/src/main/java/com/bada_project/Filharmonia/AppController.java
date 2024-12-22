8358a515414f59e1d2e1252007bd808cc9563e34
FRANCISZEK ZAREBSKI <fran.zarebski@gmail.com>
2024-12-22 13:31:34 +0100

    Add main page with login button and reservation button

--- Filharmonia/src/main/java/com/bada_project/Filharmonia/AppController.java
+++ Filharmonia/src/main/java/com/bada_project/Filharmonia/AppController.java
@@ -1,13 +1,28 @@
 package com.bada_project.filharmonia;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+    import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
 @Configuration
 public class AppController implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/index").setViewName("index");
-        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/").setViewName("main");
         registry.addViewController("/main").setViewName("main");
-        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/login").setViewName("login");
     }
 }
+
+//@Controller
+//public class AppController {
+//    @GetMapping({"/", "/index"})
+//    public String index() {
+//        return "index";
+//    }
+//
+//    @GetMapping("/main")
+//    public String main() {
+//        return "main";
+//    }
+//}
